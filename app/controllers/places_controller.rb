345e336b423be59eed7a27d84f4c9f1ce1bf453b
Sandra Harrasser <design@sandraharrasser.com>
2015-10-23 18:41:39 +0000

    pagination shows up, added redirect to form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,9 +1,24 @@
 class PlacesController < ApplicationController
 	def index
+		@places = Place.all
+	end
+	
+	def index
 		@places = Place.order(:name).page params[:page]
 	end
 	
 	def new
 		@place = Place.new
+	end
+
+	def create
+		Place.create(place_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def place_params
+		params.require(:place).permit(:name, :description, :address)
 	end
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,3 @@
 class Place < ActiveRecord::Base
-	paginates_per 10
+	paginates_per 5
 end
