4e63d82c78b908bf286c7a1a5f052c32f6e68354
Sandra Harrasser <design@sandraharrasser.com>
2015-11-04 22:43:21 +0000

    added comments controller and redirection

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Nomster::Application.routes.draw do
 
   devise_for :users
-root 'places#index'
+  root 'places#index'
 
-resources :places
+  resources :places do
+    resources :comments, :only => :create
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
