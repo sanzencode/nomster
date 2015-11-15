83add724f5383d8b0c76c835cf2fdd6eca049c63
Sandra Harrasser <design@sandraharrasser.com>
2015-11-15 03:06:47 +0000

    added my photo modal but now comments do not work.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 class CommentsController < ApplicationController
 	before_action :authenticate_user!
 
-
 	def create
 		@place = Place.find(params[:place_id])
 		@place.comments.create(comment_params.merge(:user => current_user))
