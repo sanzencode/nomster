03a61056994ae0505deba9bc7012e143c12bac35
Sandra Harrasser <design@sandraharrasser.com>
2015-11-16 22:17:02 +0000

    added mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,15 @@ Nomster::Application.configure do
   config.action_mailer.default_url_options = { :host => 'nomster-sanzencode.herokuapp.com/
 '}
   # Settings specified here will take precedence over those in config/application.rb.
-
+ ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'yourapp.heroku.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
   # Code is not reloaded between requests.
   config.cache_classes = true
 
